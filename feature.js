e44d7574bad32a636c9b48ee22f347b3bdafe7b2
kashyapAmanSingh <coder85113@gmail.com>
2023-01-05 22:18:30 +0530

    not very important

---
+++ feature.js
@@ -0,0 +1,12 @@
+<script   >   
+var type=new Typed('#typed',
+{
+    strings:['A Designer','A Designer','Coder','A You Tuber'],
+      
+    typeSpeed:150,
+backSpeed:150,
+loop:true
+
+});
+
+</script> 
